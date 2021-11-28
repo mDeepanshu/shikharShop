f757dfce32f9748d46d37061018fb7077d4a9e50
mDeepanshu <deepanshumalviya8964@gmail.com>
2021-11-29 00:19:42 +0530

    one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { Routes, RouterModule } from '@angular/router';
+
+//
+import { PurchaseComponent } from './purchase/purchase.component';
+import { AddItemComponent } from './add-item/add-item.component';
+
+const routes: Routes = [
+  { path: '', component: PurchaseComponent },
+  { path: 'additem', component: AddItemComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    PurchaseComponent,
+    AddItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatSnackBarModule,
   ],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css'],
+})
+export class SideBarComponent implements OnInit {
+  date = new Date();
+  expired = false;
+  onLink = 0;
+  onElement = 0;
+  @ViewChild('aForm') aForm: ElementRef | undefined;
+  constructor(private _snackBar: MatSnackBar, private router: Router) {}
+  routesArray = ['ledger-component'];
+  capital = ['PURCHASE'];
+  ngOnInit() {
+    const d = new Date();
+    d.setMonth(11);
+    d.setDate(30);
+    if (d.getTime() - this.date.getTime() <= 0) {
+      this.expired = true;
+      this._snackBar.open('Date Expired', 'Close');
+    } else if (d.getTime() - this.date.getTime() < 1296011173) {
+      this._snackBar.open('Software about to Expire', 'Close');
+    }
+  }
+  //
+  linkChange(r: any) {
+    this.onLink = r;
+  }
+}
